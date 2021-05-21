284a9aa3ebe7106558504edbea981de98af7b625
HyoJun-Ahn <wnsgy79@gmail.com>
2021-05-21 19:01:05 +0900

    retouch

--- Gradient descent.py
+++ Gradient descent.py
@@ -10,6 +10,8 @@ b = 0   #절편
 lrate = 0.01    #학습률
 epochs = 1000   #반복횟수
 
+n = float(len(x)) #입력 데이터의 개수
+
 for i in range(epochs):
     y_pred = W*x + b    #예측값
     dW = (2/n) * sum(x * (y_pred-y))
@@ -19,7 +21,7 @@ for i in range(epochs):
 
 print(W, b)
 #기울기와 절편을 출력한다.
-y pred = W * x + b
+y_pred = W * x + b
 #예측값을 만든다.
 plt.scatter(x, y)
 #입력 데이터를 그래프 상에 찍는다.
