ce11549b609081ca2f88e2fecab3ed520b5fb231
HyoJun-Ahn <wnsgy79@gmail.com>
2021-05-30 18:48:16 +0900

    iris

--- iris.py
+++ iris.py
@@ -1,7 +1,7 @@
 from sklearn.datasets import load_iris
 
-iris = load_iris()
-print(iris.feature_names)
-print(iris.target_names)
-print(iris.data[0])
-print(iris.target[0])
+iris = load_iris()          #iris 토이 데이터 세트를 iris변수에 저장
+print(iris.feature_names)   #특징(feature)들을 저장
+print(iris.target_names)    #학습 라벨에 해당하는 target을 출력
+print(iris.data[0])         #첫 번쨰 데이터에 저장된 feature값 출력
+print(iris.target[0])       #첫 번쨰 데이터에 저장된 target값 출력
