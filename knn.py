2d49e7a9d0ae1b2b39f59f011835dbd172c2733c
HyoJun-Ahn <wnsgy79@gmail.com>
2021-05-30 21:03:14 +0900

    knn

---
+++ knn.py
@@ -0,0 +1,8 @@
+x = [[0], [1], [2], [3], [4], [5]]
+y = [0, 0, 0, 1, 1, 1]
+from sklearn.neighbors import KNeighborsClassifier
+neigh = KNeighborsClassifier(n_neighbors=3)
+neigh.fit(x, y)
+
+print(neigh.predict([[2.8]]))
+print(neigh.predict_proba([[2.8]]))
